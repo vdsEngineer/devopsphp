d531e4cfaddff89f76b3240c8541e18b934bfb63
dvoloshenko <dmitry.voloshchenko@aquaart.group>
2026-02-20 22:40:32 +0300

    Убрал TelescopeServiceProvider из глобальных зависимостей

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local')
+            && class_exists(\App\Providers\TelescopeServiceProvider::class)) {
+            $this->app->register(\App\Providers\TelescopeServiceProvider::class);
+        }
     }
 
     /**
