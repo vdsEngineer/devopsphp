903cd58088aab35cb6192faf2df7a1ea5119af12
dvoloshenko <dmitry.voloshchenko@aquaart.group>
2026-02-20 17:02:11 +0300

    add ci-cd

---
+++ docker/k6/ci.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 10,           
+  duration: '15s',  
+
+  thresholds: {
+    'http_req_duration': ['p(95)<500'], 
+    'http_req_failed': ['rate<0.01'],   
+  },
+};
+
+export default function () {
+  const res = http.get('http://app:8000/');
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
