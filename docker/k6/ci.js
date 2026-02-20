64f1637552827e2b7d59baf9bef425eca6a0d4c0
dvoloshenko <dmitry.voloshchenko@aquaart.group>
2026-02-20 23:19:35 +0300

    fix ci-cd

--- docker/k6/ci.js
+++ docker/k6/ci.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 10,           
-  duration: '15s',  
+  vus: 50,
+  duration: '30s',
 
   thresholds: {
     'http_req_duration': ['p(95)<500'], 
